753c4881838919b534020a9fb953125dedf98a0c
Ahmed Swerky <AhmdSwerky@gmail.com>
2022-11-23 13:02:25 +0200

    chore: update cloudflare url for integration code

--- integration-code/index.js
+++ integration-code/index.js
@@ -45,4 +45,4 @@
 	//	window[windowId].fireLoaded();
 	//};
 //})('https://products-list.test', 'product-list-script', 'productLists', 'script');
-})('https://d1iigxe1h58tlc.cloudfront.net/v1', 'product-list-script', 'productLists', 'script');
+})('https://d2o3zk6c53htz2.cloudfront.net/v1', 'product-list-script', 'productLists', 'script');
